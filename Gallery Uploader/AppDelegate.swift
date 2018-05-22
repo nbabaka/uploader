4639c0065e853a9eadeb4f75610b333d547a67e0
Nikolay Karataev <babaka@cinemood.com>
2018-05-22 09:39:15 -0400

    Initial Commit

---
+++ Gallery Uploader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gallery Uploader
+//
+//  Created by Николай Каратаев on 22.05.2018.
+//  Copyright © 2018 CINEMOOD Trendsetters Co. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Gallery Uploader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Gallery Uploader
+//
+//  Created by Николай Каратаев on 22.05.2018.
+//  Copyright © 2018 CINEMOOD Trendsetters Co. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
